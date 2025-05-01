1556f4e0572a3ba29380c05d209fc069a5deed5d
Chihiro88G <1114chihiro@gmail.com>
2025-05-01 13:04:24 -0400

    Attributes using T and K concept

---
+++ src/models/Attributes.ts
@@ -0,0 +1,25 @@
+export class Attributes<T extends object> {
+  constructor(private data: T) {};
+
+  // before refactoring
+  // get(propName: string): (string | number) {
+  //   return this.data[propName];
+  // }
+
+  // T represents type
+  // K represents key. key can also be a type
+
+  // after refactoring
+  // <K extends keyof T> = generic constraints
+  // T and K in this case could be either 'name', 'age', 'id'
+  // T[K]: return an obj with K out of T 
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
+  }
+
+
+  set(update: T): void {
+    // this.data = update;
+    Object.assign(this.data, update);
+  }
+}
