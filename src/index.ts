93238afc476091e870cd0793231fcfbad6cd8715
Chihiro88G <1114chihiro@gmail.com>
2025-05-19 17:18:05 -0400

    bind events

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import { User } from "./models/User";
 import { UserForm } from "./views/UserForm";
 
+const user = User.buildUser({ name: 'NAME', age: 30 });
 
 const userForm = new UserForm(
-  document.getElementById('root')!
+  document.getElementById('root')!,
+  user
 );
 
 userForm.render();
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,6 +1,8 @@
+import { User } from "../models/User";
+
 export class UserForm {
   // Element is HTML element
-  constructor(public parent: Element) {};
+  constructor(public parent: Element, public model: User) {};
 
   eventsMap(): { [key: string]: () => void } {
     return {
@@ -16,15 +18,31 @@ export class UserForm {
     return `
       <div>
         <h1>User Form</h1>
+        <div>User name: ${this.model.get('name')}</div>
+        <div>User age: ${this.model.get('age')}</div>
         <input />
         <button>Click Me</button>
       </div>
     `;  
   };
 
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      })
+    }
+  }
+
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
+
     this.parent.append(templateElement.content);
   }
 }
